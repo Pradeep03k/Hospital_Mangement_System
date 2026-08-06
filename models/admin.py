1dc05f02538b5c4503e3ab80b067837a9b396101
Pradip Khedkar <pradip8766646528@gmail.com>
2026-08-06 20:15:31 +0530

    Delete Admin class from models/admin.py
    Remove Admin class definition from admin.py beacase it is main branch i want to protect that branch

--- models/admin.py
+++ models/admin.py
@@ -1,6 +1,1 @@
-class Admin:
-    def __init__(self, username, password, email, admin_id=None):
-        self.admin_id = admin_id
-        self.username = username
-        self.password = password
-        self.email = email
+
