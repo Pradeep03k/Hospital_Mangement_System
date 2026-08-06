c212ec46e6c133a1e48d6b10f06bcc22524e1974
pradip <pradip8766646528@gamil.com>
2026-08-06 21:10:49 +0530

    some changes made

--- menus/admin_menu.py
+++
@@ -1,6 +0,0 @@
-class Admin:
-    def __init__(self, username, password, email, admin_id=None):
-        self.admin_id = admin_id
-        self.username = username
-        self.password = password
-        self.email = email
